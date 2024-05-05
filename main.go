145ad98db88f69e0ac97bfdc4ce0108d291b27c9
Jay_local <go.jhson@gmail.com>
2024-05-06 08:05:59 +0900

    Create a shuffle mode

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
+	"math/rand"
 	"os"
 	"strings"
 	"time"
@@ -15,15 +16,18 @@ type problem struct {
 }
 
 func main() {
+	// Init flags
 	fn := flag.String("csv", "problems.csv", "CSV file path format (question, answer)")
 	timer := flag.Int("timer", 30, "Set timer for each question, default value (30seconds)")
+	shuffle := flag.Bool("shuffle", false, "Shuffle question order, default value: false")
 	flag.Parse()
+
+	// Open file and decode csv data
 	f, err := os.Open(*fn)
 	if err != nil {
 		fmt.Printf("Failed to open file: '%s', %v\n", *fn, err)
 		os.Exit(1)
 	}
-
 	data := csv.NewReader(f)
 	rec, err := data.ReadAll()
 	if err != nil {
@@ -31,31 +35,50 @@ func main() {
 		os.Exit(1)
 	}
 
-	p := make([]problem, len(rec))
+	pLen := len(rec)
+	correct := 0
+	ch := make(chan int)
+
+	p := make([]problem, pLen)
 	if err := csvParser(rec, p); err != nil {
 		fmt.Printf("Failed to parse CSV file / %e\n", err)
 		os.Exit(1)
 	}
 
-	var correct int
+	// Start timer after confirm
+	fmt.Println("after chanel insert")
+	fmt.Print("Press 'enter' to start quiz and timer")
+	fmt.Scanln()
+	timeLimit := time.NewTimer(time.Second * time.Duration(*timer))
+	defer timeLimit.Stop()
+
+	// Make it rand order if toggled
+	ps := make([]int, pLen)
+	switch {
+	case *shuffle:
+		ps = rand.Perm(pLen)
+	default:
+		for i := range pLen {
+			ps[i] = i
+		}
+	}
 L:
-	for i, row := range p {
-		ch := make(chan int)
-		fmt.Print("Press 'enter' to start quiz and timer")
-		fmt.Scanln()
-		fmt.Printf("Q%d. %s=", i+1, row.q)
+	for i, pNum := range ps {
+		fmt.Printf("Q%d. %s=", i+1, p[pNum].q)
+		// time.NewTimer(time.Second * time.Duration(*timer))
 
 		go func() {
 			var a string
 			fmt.Scan(&a)
 			ch <- 1
-			if strings.TrimSpace(a) == row.a {
+			if strings.TrimSpace(a) == p[pNum].a {
 				correct++
 			}
 		}()
 		select {
 		case <-ch:
-		case <-time.After(time.Second * time.Duration(*timer)):
+			// No matter time after chanel is empty
+		case <-timeLimit.C:
 			fmt.Println("\nPass time limit")
 			break L
 		}
@@ -75,5 +98,4 @@ func csvParser(input [][]string, output []problem) error {
 		output[i].a = strings.TrimSpace(row[1])
 	}
 	return nil
-
 }
